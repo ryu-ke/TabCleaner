519fda53a8a18997290cdb6f113eb1c75430073f
ryunosuke <ryunosuke.iwai@gmail.com>
2019-07-01 14:48:06 +0900

    Create prototype

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,19 +1,45 @@
-function loadActiveLog(): {} {
-    return JSON.parse(window.localStorage.getItem('active_log') || "{}");
+enum StoreKeys {
+    ActiveLog = "active_log",
+    ActiveTabId = "active_tab_id",
 }
 
-function saveActiveLog(log: {}) {
-    window.localStorage.setItem('active_log', JSON.stringify(log))
-}
-
-function getNowTimestamp(): number {
+function nowTimestamp(): number {
     const date = new Date();
-    return Math.floor(date.getTime() / 1000);
+    return Math.floor(date.getTime() / 1000);    
 }
 
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    var log = loadActiveLog();
-    log[activeInfo.tabId] = getNowTimestamp();
-    console.log(JSON.stringify(log));
-    saveActiveLog(log);
-})
+chrome.tabs.onActivated.addListener(function(activeInfo: chrome.tabs.TabActiveInfo): void {
+    var activeLog = JSON.parse(window.localStorage.getItem(StoreKeys.ActiveLog) || "{}");
+    const oldTabId = Number(window.localStorage.getItem(StoreKeys.ActiveTabId));
+    const newTabId = activeInfo.tabId;
+    const now = nowTimestamp();
+
+    activeLog[oldTabId] = now;
+    activeLog[newTabId] = now;
+
+    window.localStorage.setItem(StoreKeys.ActiveLog, JSON.stringify(activeLog));
+    window.localStorage.setItem(StoreKeys.ActiveTabId, String(newTabId));
+});
+
+chrome.alarms.create("check", { "periodInMinutes": 1 });
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    chrome.tabs.query({}, function(tabs: chrome.tabs.Tab[]): void {
+        const tabIds = tabs.map(function(tab: chrome.tabs.Tab): number {
+            return tab.id
+        });
+
+        const activeLog = JSON.parse(window.localStorage.getItem(StoreKeys.ActiveLog) || "{}");
+
+        return tabIds.forEach(function(tabId: number) {
+            const now = nowTimestamp();
+            const unusedSeconds = now - (activeLog[tabId] || now);
+
+            console.log(`${tabId}: ${unusedSeconds}`);
+
+            // if (unusedSeconds >= 5 * 60) {
+            //     chrome.tabs.remove(tabId);
+            // }
+        });
+    });
+});
